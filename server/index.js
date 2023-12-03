abf9e827a478f4fc0d23688fdd845eb0383fde19
Alan <28246519+axpk@users.noreply.github.com>
2023-12-02 19:41:45 -0800

    Added urlencoded extended false as default is deprecated (#5)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const db = process.env.ATLAS_URI;
 
 // Routes
