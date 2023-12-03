2bc84bd70a384b43172938add3d297972397ce1d
Matthew Wong <matthewcwwong01@gmail.com>
2023-12-02 22:31:04 -0800

    added profile page and changed login/signup css

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./components/Login";
 import Signup from "./components/Signup.js";
 import BookTickets from "./components/BookTickets.js";
 import Home from "./pages/home-page/Home.js";
+import Profile from "./components/Profile.js";
 import './style.css'
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
             <Route path="/login" element={<Login/>} />
             <Route path="/signup" element={<Signup/>} />
             <Route path="/booktickets" element={<BookTickets/>} />
+            <Route path="/profile" element={<Profile/>} />
             </Routes>
         </div>
     );
