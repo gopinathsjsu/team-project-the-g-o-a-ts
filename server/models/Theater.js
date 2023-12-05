3e9ba4a24bc5a3bfd231d9eb98e10bf50bd1d574
Alan <28246519+axpk@users.noreply.github.com>
2023-12-04 21:16:46 -0800

    Added capacity to model (#24)

--- server/models/Theater.js
+++ server/models/Theater.js
@@ -4,6 +4,7 @@ const theaterSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
   screens: { type: Number, required: true },
+  capacity: { type: Number, requierd: true },
 });
 
 module.exports = mongoose.model("Theater", theaterSchema, "theaters");
